76b9dceaae9c205d42f3b5f6fde51836eed2ee8c
xch <xch@qq.com>
2018-06-01 19:21:11 +0800

    add xxl-job module

---
+++ java-framework-xxl-job/xxl-job-quick-start/src/main/java/com/bestcode/xxljob/JobApplication.java
@@ -0,0 +1,20 @@
+package com.bestcode.xxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot启动类
+ *
+ * @author <a href="mailto:1205241831@qq.com">Xch</a>
+ * @version 1.00
+ * @see
+ * @since 2018.06.01
+ */
+@SpringBootApplication
+public class JobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
+}
