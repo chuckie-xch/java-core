80aee64c4d08e6ebad23b246eaaa3f843ece80c2
chuckie-xch <1205241831@qq.com>
2018-06-06 23:26:47 +0800

    add mr-product

---
+++ java-framework-spring-cloud/mr-product/src/test/java/com/bestcode/mr/product/repo/ProductCategoryRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.bestcode.mr.product.repo;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.bestcode.mr.product.model.entity.ProductCategory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryRepositoryTest {
+
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+
+    @Test
+    public void findByCategoryTypeIn() {
+        List<ProductCategory> list = productCategoryRepository.findByCategoryTypeIn(Arrays.asList(11, 12));
+        Assert.assertTrue(!list.isEmpty());
+    }
+}
