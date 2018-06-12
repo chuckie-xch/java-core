64e200a4ecf424ccae5b4e2f4ba7445ba092969c
chuckie-xch <1205241831@qq.com>
2018-06-12 23:30:35 +0800

    complete create order

--- java-framework-spring-cloud/mr-order/src/main/java/com/bestcode/mr/order/service/impl/OrderServiceImpl.java
+++ java-framework-spring-cloud/mr-order/src/main/java/com/bestcode/mr/order/service/impl/OrderServiceImpl.java
@@ -1,11 +1,18 @@
 package com.bestcode.mr.order.service.impl;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.bestcode.mr.order.client.ProductClient;
 import com.bestcode.mr.order.enums.OrderStatusEnum;
 import com.bestcode.mr.order.enums.PayStatusEnum;
+import com.bestcode.mr.order.model.dto.CartDTO;
 import com.bestcode.mr.order.model.dto.OrderDTO;
+import com.bestcode.mr.order.model.entity.OrderDetail;
 import com.bestcode.mr.order.model.entity.OrderMaster;
+import com.bestcode.mr.order.model.entity.ProductInfo;
 import com.bestcode.mr.order.repo.OrderDetailRepository;
 import com.bestcode.mr.order.repo.OrderMasterRepository;
 import com.bestcode.mr.order.service.OrderService;
@@ -27,21 +34,46 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderMasterRepository orderMasterRepository;
 
+    @Autowired
+    private ProductClient productClient;
+
     /**
      * 创建订单
+     *
      * @param orderDTO
      * @return
      */
+    @Override
     public OrderDTO create(OrderDTO orderDTO) {
-
-        // TODO 查询商品信息
-        // TODO 计算总价
-        // TODO 扣库存
+        String orderId = KeyUtil.uuid();
+        // 查询商品信息
+        List<String> productIdList = orderDTO.getOrderDetailList().stream().map(OrderDetail::getProductId).collect
+                (Collectors.toList());
+        List<ProductInfo> productInfoList = productClient.listForOrder(productIdList);
+        // 计算总价
+        BigDecimal orderAmount = BigDecimal.ZERO;
+        for (OrderDetail orderDetail : orderDTO.getOrderDetailList()) {
+            for (ProductInfo productInfo : productInfoList) {
+                if (productInfo.getProductId().equals(orderDetail.getProductId())) {
+                    orderAmount = productInfo.getProductPrice().multiply(new BigDecimal(orderDetail
+                            .getProductQuantity())).add(orderAmount);
+                    BeanUtils.copyProperties(productInfo, orderDetail);
+                    orderDetail.setOrderId(orderId);
+                    orderDetail.setDetailId(KeyUtil.uuid());
+                    // 订单详情入库
+                    orderDetailRepository.save(orderDetail);
+                }
+            }
+        }
+        // 扣库存
+        List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream().map(e -> new CartDTO(e.getProductId(), e
+                .getProductQuantity())).collect(Collectors.toList());
+        productClient.decreaseStock(cartDTOList);
         // 订单入库
         OrderMaster orderMaster = new OrderMaster();
-        orderDTO.setOrderId(KeyUtil.uuid());
+        orderDTO.setOrderId(orderId);
         BeanUtils.copyProperties(orderDTO, orderMaster);
-        orderMaster.setOrderAmount(new BigDecimal(200));
+        orderMaster.setOrderAmount(orderAmount);
         orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
         orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
         orderMasterRepository.save(orderMaster);
