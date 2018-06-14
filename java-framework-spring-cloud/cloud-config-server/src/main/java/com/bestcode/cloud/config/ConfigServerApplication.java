efe6f73a92635bfadc251ae2c81346e4675c65a6
chuckie-xch <1205241831@qq.com>
2018-06-14 22:55:49 +0800

    add config server

---
+++ java-framework-spring-cloud/cloud-config-server/src/main/java/com/bestcode/cloud/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.bestcode.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xch
+ * @create 2018-06-14 22:30
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
