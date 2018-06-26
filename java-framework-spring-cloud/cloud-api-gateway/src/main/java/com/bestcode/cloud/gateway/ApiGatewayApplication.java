cec84e0f1b2408832f65d3a181d74292e7d9e905
chuckie-xch <1205241831@qq.com>
2018-06-26 21:53:34 +0800

    添加api网关zuul

---
+++ java-framework-spring-cloud/cloud-api-gateway/src/main/java/com/bestcode/cloud/gateway/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.bestcode.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author xch
+ * @create 2018-06-26 21:16
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
