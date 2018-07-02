064fa5fe2df94366e87636ce6869456e183f8220
chuckie-xch <1205241831@qq.com>
2018-07-02 20:55:32 +0800

    zuul限流实现

---
+++ java-framework-spring-cloud/cloud-api-gateway/src/main/java/com/bestcode/cloud/gateway/exception/RateLimitException.java
@@ -0,0 +1,10 @@
+package com.bestcode.cloud.gateway.exception;
+
+/**
+ * 限流异常
+ *
+ * @author xch
+ * @create 2018-07-02 20:51
+ **/
+public class RateLimitException extends RuntimeException {
+}
---
+++ java-framework-spring-cloud/cloud-api-gateway/src/main/java/com/bestcode/cloud/gateway/filter/RateLimitFilter.java
@@ -0,0 +1,43 @@
+package com.bestcode.cloud.gateway.filter;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SERVLET_DETECTION_FILTER_ORDER;
+
+import com.bestcode.cloud.gateway.exception.RateLimitException;
+import com.google.common.util.concurrent.RateLimiter;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ * 限流拦截器
+ *
+ * @author xch
+ * @create 2018-07-02 20:42u
+ **/
+public class RateLimitFilter extends ZuulFilter{
+
+    private static final RateLimiter RATE_LIMITER = RateLimiter.create(100);
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SERVLET_DETECTION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        if (!RATE_LIMITER.tryAcquire()) {
+            throw new RateLimitException();
+        }
+        return null;
+    }
+}
