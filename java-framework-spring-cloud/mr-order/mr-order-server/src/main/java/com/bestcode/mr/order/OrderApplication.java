80ebb537bc84be3ab1568281810d7b6ef8bf5e73
chuckie-xch <1205241831@qq.com>
2018-07-04 22:13:08 +0800

    hystrix服务降级和熔断

--- java-framework-spring-cloud/mr-order/mr-order-server/src/main/java/com/bestcode/mr/order/OrderApplication.java
+++ java-framework-spring-cloud/mr-order/mr-order-server/src/main/java/com/bestcode/mr/order/OrderApplication.java
@@ -3,20 +3,25 @@ package com.bestcode.mr.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.retry.annotation.CircuitBreaker;
 
 /**
  * @author xch
  * @create 2018-06-07 23:28
  **/
-@SpringBootApplication
-@EnableEurekaClient
+
 @EnableFeignClients(basePackages = "com.bestcode.product.client")
 @EnableBinding({Sink.class, Source.class})
+//@CircuitBreaker
+//@SpringBootApplication
+//@EnableEurekaClient
+@SpringCloudApplication
 public class OrderApplication {
 
     public static void main(String[] args) {
---
+++ java-framework-spring-cloud/mr-order/mr-order-server/src/main/java/com/bestcode/mr/order/controller/HystrixController.java
@@ -0,0 +1,54 @@
+package com.bestcode.mr.order.controller;
+
+import java.util.Arrays;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author xch
+ * @create 2018-07-04 21:02
+ **/
+@RestController
+@RequestMapping("/hystrix")
+@DefaultProperties(defaultFallback = "defaultFallback")
+public class HystrixController {
+
+    @GetMapping("/getProductInfoList")
+//    @HystrixCommand(fallbackMethod = "fallback")
+    /**
+     * 超时配置
+     *
+     * @HystrixCommand(commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",
+     * value = "3000")})
+     */
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),// 设置熔断
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "1000"),
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
+    })
+    public String getProductInfoList(@RequestParam("number") Integer number) {
+        if (number % 2 == 0) {
+            return "success";
+        }
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject("http://127.0.0.1:8080/product/listForOrder", Arrays.asList
+                        ("157875196366160022"),
+                String.class);
+    }
+
+    private String fallback() {
+        return "太拥挤了，请稍后再试~~";
+    }
+
+    private String defaultFallback() {
+        return "默认降级提示：服务繁忙，稍后重试~~";
+    }
+}
